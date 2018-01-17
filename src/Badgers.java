202a91a8719031b7a7d2b5feac49a78b49a7639d
cunningham.callum <cunningham.callum@SEMS-259-07.SEMS259.LAB>
2018-01-17 14:41:45 -0800

    Badgers

---
+++ src/Badgers.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class Badgers {
+
+	public static void main(String[] args) {
+		
+		for(int j =0;j<3;j++) {
+	
+		for(int i =0;i<12;i++) {
+			System.out.print("Badger, ");
+			
+			
+		}
+		
+		System.out.println();
+		
+		for(int i =0;i<2;i++) {
+			System.out.print("Mushroom, ");
+			
+		
+	}
+		
+		System.out.println();
+		
+		
+		
+}
+
+		for(int i =0;i<5;i++) {
+			System.out.print("Snake, ");
+		
+}
+}
+}
